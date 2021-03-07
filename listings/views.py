fd23f72fa562e91d625985e357d3015873be4883
nabily4e-dev <nbl.dv21@gmail.com>
2021-03-08 02:37:28 +0300

    Adding pagination!

--- listings/views.py
+++ listings/views.py
@@ -1,18 +1,26 @@
-from django.db import models
 from django.shortcuts import render
 from .models import Listing
-from .models import *
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def index(request):
-    return render(request, 'listings/listings.html', {
-        'name': 'NabiL'
-    })
+    
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    
+    
+    context = {
+        'listings': paged_listings
+    }
+    
+    return render(request, 'listings/listings.html', context, )
 
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+def listing(request, listing_id):
+    return render(request, 'listings/listing.html', )
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    return render(request, 'listings/search.html', )
