279e8eb70000d5e9c832aadedb1425040144e58a
nabily4e-dev <nbl.dv21@gmail.com>
2021-03-07 15:32:06 +0300

    Done with the admin UI

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django.db import models
 from .models import Realtor
 
 
-admin.site.register(Realtor)
+class RealtorsAdmin(admin.ModelAdmin):
+    """"""
+    
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'name')
+    search_fields = ('name')
+    list_per_page = 25
+
+
+admin.site.register(Realtor, RealtorsAdmin)
+
+
