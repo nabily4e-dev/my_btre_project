1f3a72f13e2bbb4068febdff6d268f7332473a2c
nabily4e-dev <nbl.dv21@gmail.com>
2021-03-07 10:59:52 +0300

    Migration complete

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from datetime import datetime
+from realtors.models import Realtor
 
-# Create your models here.
+
+class Listing(models.Model):
+    """Sitting up the Listing model scheme"""
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=200)
+    description = models.TextField(blank=True) # blank=True parameter is set to make it optional rather than required!
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+    garage = models.IntegerField(default=0)
+    bedrooms = models.IntegerField()
+    square_feet = models.IntegerField()
+    lot_size = models.DecimalField(max_digits=5, decimal_places=1)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%m/%d/')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%m/%d/', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
+    
+    
