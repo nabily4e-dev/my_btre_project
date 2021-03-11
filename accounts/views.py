b3bb346ebd57b96253879d8c953af6f6ecd307a9
nabily4e-dev <nbl.dv21@gmail.com>
2021-03-11 16:44:41 +0300

    adding users and authentication!

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,59 @@
-
 from django.shortcuts import redirect, render
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 
 def register(request):
     """"""
-    
+
     if request.method == 'POST':
-        # Register User
-        print('SUBMITTED REG')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if passwords match
+        if password == password2:
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'That email is being used')
+                return redirect('register')
+            else:
+                # Create User
+                user = User.objects.create_user(username=username,
+                                                password=password,
+                                                email=email,
+                                                first_name=first_name,
+                                                last_name=last_name)
+                # Login after register
+                # auth.login(request, user)
+                # messages.success(request, 'Logedin success')
+                # redirect('index')
+                
+                user.save()
+                messages.success(request, 'You are now registered and can log in')
+                return redirect('login')
+                
+        else:
+            messages.error(request, 'Password do not match')
+            return redirect('register')
+
+        # # Register User
+        # messages.error(request, 'Testing error message')
+        # return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
 
 def login(request):
     """"""
-    
+
     if request.method == 'POST':
         # Login User
         print('SUBMITTED REG')
@@ -26,14 +64,11 @@ def login(request):
 
 def logout(request):
     """"""
-    
+
     return redirect(request, 'index')
 
 
 def dashboard(request):
     """"""
-    
-    
+
     return render(request, 'accounts/dashboard.html')
-
-
--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,3 @@
-const date = new Date();
-document.querySelector('.year').innerHTML = date.getFullYear();
+setTimeout(function () {
+    $('#message').fadOut('slow');
+}, 3000);
